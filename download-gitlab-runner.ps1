ff9c0e4e61e29630bced75c1626b8d1a80566e3d
Czarnecki Maciej <maciej.czarnecki@ts.fujitsu.com>
2018-02-12 14:06:08 +0100

    Changed download method

--- download-gitlab-runner.ps1
+++ download-gitlab-runner.ps1
@@ -1,2 +1,3 @@
-$WebClient = New-Object System.Net.WebClient;
-$WebClient.DownloadFile('https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe', 'gitlab-runner.exe');
+Write-Output "Downloading git runner..."
+Invoke-WebRequest -Uri 'https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe' -OutFile 'gitlab-runner.exe';
+Write-Output "Finieshed."
--- install-java.ps1
+++ install-java.ps1
@@ -1,10 +1,13 @@
-$WebClient = New-Object System.Net.WebClient;
-$Cookie = "oraclelicense=accept-securebackup-cookie";
-$WebClient.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie);
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
+$oracle_cookie = New-Object System.Net.Cookie ;
+$oracle_cookie.Name = "oraclelicense";
+$oracle_cookie.Value = "accept-securebackup-cookie";
+$oracle_cookie.Domain = ".oracle.com";
+$session.Cookies.Add($oracle_cookie);
 Write-Output "Downloading JRE..."
-$WebClient.DownloadFile('http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jre-9.0.4_windows-x64_bin.exe', 'jre_windows-x64_bin.exe') ;
+Invoke-WebRequest -Uri 'http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jre-9.0.4_windows-x64_bin.exe' -WebSession $session -OutFile 'jre_windows-x64_bin.exe'
 Write-Output "Installing JRE..."
 Start-Process ./jre_windows-x64_bin.exe -ArgumentList '/s' -Wait;
 Write-Output "Removing installation package"
-Remove-Item ./jre_windows-x64_bin.exe
+#Remove-Item ./jre_windows-x64_bin.exe
 Write-Output "Finieshed."
